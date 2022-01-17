63940d1e2cd1b121c05c3e14babe30a29ebe420e
Omar Jammoul <omarjammoul20@gmail.com>
2022-01-17 17:55:00 +0400

    First python

---
+++ How to Python.py
@@ -0,0 +1,10 @@
+print("Hello Python")
+
+name = "Omar"
+surname = "Jammoul"
+print(name + " " + surname)
+
+user_input = input("Please enter your course name")
+print(type(user_input))
+print(user_input)
+
